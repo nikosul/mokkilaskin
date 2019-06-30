8e2990db4860aa83081e51f7d54f34a15071230e
jea <a1800601@myy.haaga-helia.fi>
2019-06-30 20:15:08 +0300

    1.0

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myapplication
+
+import android.graphics.Color
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.text.Typography.euro
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+    fun doMath(v: View) {
+
+
+        val getDestination = findViewById<EditText>(R.id.destination_input)
+        val totalDays = findViewById<EditText>(R.id.days_input)
+        val totalFood = findViewById<EditText>(R.id.food_input)
+        val totalPeople = findViewById<EditText>(R.id.people_input)
+        val getResults = findViewById<TextView>(R.id.getResult)
+
+        if (totalPeople.text.toString().isNotEmpty() && getDestination.text.toString().isNotEmpty() && totalDays.text.toString().isNotEmpty()) {
+
+            val peopleParse = java.lang.Double.parseDouble(totalPeople.text.toString())
+            val costParse = java.lang.Double.parseDouble(totalFood.text.toString())
+            val euronmerkki = euro
+            val jako = "Cost for each "
+
+            val total = String.format("%.2f", costParse / peopleParse).toDouble()
+            getResults.text = jako + total.toString() + euronmerkki
+        }
+    }
+}
